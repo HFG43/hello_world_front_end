94a889b0fb4607384047ca55873d7fbd0c016170
HFG43 <hernanguemes@hotmail.com>
2023-11-07 20:26:28 -0300

    Misc: linters

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
